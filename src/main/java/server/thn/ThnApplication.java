2bc0a2d8ad3cff960fe24961d2ef91597944b8d5
myway00 <junior0614@naver.com>
2022-09-19 16:24:08 +0900

    #1 init : Made THN Project with default setting

---
+++ src/main/java/server/thn/ThnApplication.java
@@ -0,0 +1,13 @@
+package server.thn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/thn/ThnApplicationTests.java
@@ -0,0 +1,13 @@
+package server.thn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
